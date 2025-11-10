1be5e44a5f5b832951edd86262c14932544e5de5
anik33 <anik.cse.nu@gmail.com>
2025-11-10 14:03:32 +0600

    connect to mongodb and create environment and get all car data from modelcollection

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const cors = require("cors");
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = 3000
+
+
+app.use(cors());
+app.use(express.json());
+
+// dU86G1UY6Q9t6ycD
+// car-wheels-db
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cjfssbu.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('cars-db')
+    const modelCollection = db.collection('cars')
+
+    app.get('/allcar',async(req,res)=>{
+
+        const result = await modelCollection.find().toArray()
+
+        res.send(result)
+
+    })
+
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
