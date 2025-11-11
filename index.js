60b6d408febf124048aa5cbe4faf937ed9d23475
anik33 <anik.cse.nu@gmail.com>
2025-11-11 19:30:52 +0600

    search api,my listing api,find by id apis created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = 3000
 
@@ -17,69 +17,118 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    await client.connect();
+    try {
+        await client.connect();
 
-    const db = client.db('cars-db')
-    const carCollection = db.collection('cars')
-    const testimonial =db.collection('testimonial')
-    // get method
-    app.get('/allcar',async(req,res)=>{
+        const db = client.db('cars-db')
+        const carCollection = db.collection('cars')
+        const testimonial = db.collection('testimonial')
+        // get method
+        app.get('/allcar', async (req, res) => {
 
-        const result = await carCollection.find().toArray()
+            const result = await carCollection.find().toArray()
 
-        res.send(result)
+            res.send(result)
 
-    })
-    // get method for testimonial
-    app.get('/testimonial',async(req,res)=>{
+        })
+        // get method for testimonial
+        app.get('/testimonial', async (req, res) => {
 
-        const result = await testimonial.find().toArray()
+            const result = await testimonial.find().toArray()
 
-        res.send(result)
+            res.send(result)
 
-    })
+        })
 
-  // post method
-   app.post("/allcar", async(req,res)=>{
-    const data = req.body;
-    const result = await carCollection.insertOne(data);
-    res.send(result)
-   })
-    
-
+        // post method
+        app.post("/allcar", async (req, res) => {
+            const data = req.body;
+            const result = await carCollection.insertOne(data);
+            res.send(result)
+        })
 
 
+    // find single car
+     app.get("/car/:id",  async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
 
+      const result = await carCollection.findOne({ _id: objectId });
 
-
-    // latest 6 data
-    app.get("/latest-cars", async (req, res) => {
-      const result = await carCollection
-        .find()
-        .sort({ CreatedAt: "desc" })
-        .limit(6)
-        .toArray();
-
-      console.log(result);
-
-      res.send(result);
+      res.send({
+        success: true,
+        result,
+      });
     });
-    
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+
+
+        // latest 6 data
+        app.get("/latest-cars", async (req, res) => {
+            const result = await carCollection
+                .find()
+                .sort({ CreatedAt: "desc" })
+                .limit(6)
+                .toArray();
+
+            console.log(result);
+
+            res.send(result);
+        });
+
+        // search car api
+        app.get("/search", async (req, res) => {
+            const search_text = req.query.search
+            const result = await carCollection.find({ CarName: { $regex: search_text, $options: "i" } }).toArray()
+            res.send(result)
+        });
+
+        // My Listing api ,find data through your email
+
+        app.get("/my-listing", async (req, res) => {
+            const email = req.query.email
+            const result = await carCollection.find({ ProviderEmail: email }).toArray()
+            res.send(result)
+        })
+
+    //PUT APIS
+
+    app.put("/car/:id",  async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      // console.log(id)
+      // console.log(data)
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: data,
+      };
+
+      const result = await carCollection.updateOne(filter, update);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -87,9 +136,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
